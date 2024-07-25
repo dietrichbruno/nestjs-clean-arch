c9a5157c88a5d79bc4d620faf0d23c2d380fba42
Bruno Dietrich Schmitz <bruno.dietrich@appmax.com.br>
2024-07-25 11:01:13 -0300

    Adding search method in InMemorySerchableRepository - Part 2

--- src/shared/domain/repositories/in-memory.searchable.repository.ts
+++ src/shared/domain/repositories/in-memory.searchable.repository.ts
@@ -10,22 +10,44 @@ export abstract class InMemorySearchableRepository<E extends Entity>
   extends InMemoryRepository<E>
   implements SearchableRepositoryInterface<E, any, any>
 {
-  async search(props: SearchParams): Promise<SearchResult<E>> {}
+  async search(props: SearchParams): Promise<SearchResult<E>> {
+    const itemsFiltered = await this.applyFilter(this.items, props.filter);
+    const itemsSorted = await this.applySort(
+      itemsFiltered,
+      props.sort,
+      props.sortDir,
+    );
+    const itemsPaginated = await this.applyPaginate(
+      itemsSorted,
+      props.page,
+      props.perPage,
+    );
+
+    return new SearchResult({
+      items: itemsPaginated,
+      total: itemsFiltered.length,
+      currentPage: props.page,
+      perPage: props.perPage,
+      sort: props.sort,
+      sortDir: props.sortDir,
+      filter: props.filter,
+    });
+  }
 
   protected abstract applyFilter(
     items: E[],
     filter: string | null,
   ): Promise<E[]>;
 
-  protected abstract applySort(
+  protected async applySort(
     items: E[],
     sort: string | null,
     sortDir: string | null,
-  ): Promise<E[]>;
+  ): Promise<E[]> {}
 
-  protected abstract applyPaginate(
+  protected async applyPaginate(
     items: E[],
     page: SearchParams['page'],
     perPage: SearchParams['perPage'],
-  ): Promise<E[]>;
+  ): Promise<E[]> {}
 }
