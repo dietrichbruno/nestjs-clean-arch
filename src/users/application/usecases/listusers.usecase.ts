38da221a27d89af82a2601c82e5be4c41d636a96
Bruno Dietrich Schmitz <bruno.dietrich@appmax.com.br>
2024-07-30 11:33:12 -0300

    Adding ListUsers UseCase - Part 1

--- src/users/application/usecases/listusers.usecase.ts
+++ src/users/application/usecases/listusers.usecase.ts
@@ -1,21 +1,21 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 import { UserOutput } from '../dtos/user-output';
 import { UseCase as DefaultUseCase } from '@/shared/application/use-case';
+import { SearchInput } from '@/shared/application/dtos/search-input';
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
-export namespace GetUserUseCase {
-  export type Input = {
-    id: string;
-  };
+export namespace ListUsersUseCase {
+  export type Input = SearchInput;
 
-  export type Output = UserOutput;
+  export type Output = void;
 
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private userRepository: UserRepository.Repository) {}
 
     async execute(input: Input): Promise<Output> {
-      const entity = await this.userRepository.findById(input.id);
-      return entity.toJSON();
+      const params = new UserRepository.SearchParams(input);
+      const searchResult = await this.userRepository.search(params);
+      return;
     }
   }
 }
